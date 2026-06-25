41e45d8ff8304cf80c7a0c8c3e740ae1328f41d7
Vladyslav <vladyslav.krutii.25@kse.org.ua>
2026-06-25 17:43:38 +0300

    create two procedure(Create New Order and Add Product to Order)

--- Practical_Assignments_3/structure_db.sql
+++ Practical_Assignments_3/structure_db.sql
@@ -40,9 +40,48 @@ create table order_log (
 -- task 1 
 
 create or replace function calculate_order_total(p_order_id int)
-returns numeric(10,2) 
+returns decimal(10,2) 
 as $$
     select coalesce(sum(quantity * price), 0)
     from order_items
     where order_id = p_order_id;
 $$ language sql;
+
+-- task 2
+create or replace procedure create_order(p_customer_id int)
+as $$
+begin
+    insert into orders(customer_id, order_date, total_amount)
+    values(p_customer_id, current_timestamp, 0);
+end;
+$$ language plpgsql;
+
+-- task 3
+create or replace procedure add_product_to_order(
+    p_order_id int,
+    p_product_id int,
+    p_quantity int
+)
+as $$
+declare
+    v_price decimal(10,2);
+    v_stock int;
+begin
+    if p_quantity < 0 then raise exception 'error quantity';
+    end if;
+
+    select price, stock_quantity into v_price, v_stock 
+    from products 
+    where product_id = p_product_id;
+
+    if v_stock < p_quantity then raise exception 'no stock';
+    end if;
+
+    insert into order_items(order_id, product_id, quantity, price)
+    values(p_order_id, p_product_id, p_quantity, v_price);
+
+    update products 
+    set stock_quantity = stock_quantity - p_quantity 
+    where product_id = p_product_id;
+end;
+$$ language plpgsql;
